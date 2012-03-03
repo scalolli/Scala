73097a187f0cbf9693c6fee51a9ca2dac62b9177
Basavaraj <basavarajkalloli@gmail.com>
2012-03-03 15:34:40 +0530

    My first scala program.

---
+++ src/main/helloworld/HelloWorld.scala
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Basu
+ * Date: 3/3/12
+ * Time: 3:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object HelloWorld {
+  def main (args: Array[String]) {
+    println("Hello World")
+  }
+}
+
